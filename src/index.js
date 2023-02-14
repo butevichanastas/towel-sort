c82981afb49d335e78d9fa341e3d110595215830
butevichanastas <118684036+butevichanastas@users.noreply.github.com>
2023-02-14 17:47:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if(!Array.isArray(matrix) || matrix.length === 0)
   return [];
+  else 
+  (
+    const evenArray = matrix.filter((item, index) => index % 2)
+  )
+
 }
