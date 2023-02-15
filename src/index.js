eac1808f8185e39792c4aa1b18e62adeb9deb217
butevichanastas <butevichanastas@gmail.com>
2023-02-15 20:56:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  if(!Array.isArray(matrix) || matrix.length === 0)
-  return [];
-  else 
-  (
-    const evenArray = matrix.filter((item, index) => index % 2)
-  )
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix) || matrix.length === 0) return [];
 
+  const reversedArr =  matrix.map((item, index) => {
+  if(index % 2){
+  return item.reverse()
+  } else {
+  return item
+  }
+    
+  })
+  return reversedArr.flat()
 }
